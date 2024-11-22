dfea8aaa08fc6bff174d855abeb3c83a90e04c98
Sumair javed <sumairjaved007@gmail.com>
2024-11-22 17:57:01 +0500

    Harvard lecture start

--- pythoncoding/pythoncoding.py
+++ pythoncoding/pythoncoding.py
@@ -904,36 +904,75 @@
 
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class faculty:
-    def __init__(self):
-        self.id = int(input("Enter faculty ID: "))
-        self.name = input("Enter name: ")
-        self.salary = float(input("Enter faculty salary: "))
+# class faculty:
+#     def __init__(self):
+#         self.id = int(input("Enter faculty ID: "))
+#         self.name = input("Enter name: ")
+#         self.salary = float(input("Enter faculty salary: "))
 
-    def display(self):
-        print('Faculty Id : ', self.id)
-        print('Faculty Name : ', self.name)
-        print('Faculty Salary : ', self.salary)
+#     def display(self):
+#         print('Faculty Id : ', self.id)
+#         print('Faculty Name : ', self.name)
+#         print('Faculty Salary : ', self.salary)
         
 
-faculty1 = faculty()
-print()
-print(faculty1.id)
-print(faculty1.name)
-print(faculty1.salary)
+# faculty1 = faculty()
+# print()
+# print(faculty1.id)
+# print(faculty1.name)
+# print(faculty1.salary)
 
-while True:  
-    if faculty1.salary <= 20000:
-        print("Job level: Internship")
-        break
-    elif faculty1.salary <= 60000:
-        print("Job level: Mid")
-        break
-    elif faculty1.salary >= 60000:
-        print("Job level: Senior")
-        break    
-    else:
-        print("Type the correst information")
+# while True:  
+#     if faculty1.salary <= 20000:
+#         print("Job level: Internship")
+#         break
+#     elif faculty1.salary <= 60000:
+#         print("Job level: Mid")
+#         break
+#     elif faculty1.salary >= 60000:
+#         print("Job level: Senior")
+#         break    
+#     else:
+#         print("Type the correst information")
 
 
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+#arrays
+#linked list
+
+class Node:
+    def __init__(self, data= None, next=None):
+        self.data = data
+        self.next = next
+
+class linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        llstr = ''
+
+        itr = self.head
+        while itr:
+            llstr += str(itr.data) + '-->'
+            itr = itr.next
+
+        print(llstr)
+
+if __name__ == '__main__':
+    ll = linkedlist()
+    ll.insert_at_begining(5)
+    ll.insert_at_begining(666)
+    ll.print()
+
+
+#//////////////////////////////////
