35943e7f8b56be1b6b6a7d49fb046ac5fd983a0a
Sumair javed <sumairjaved007@gmail.com>
2024-11-29 16:56:42 +0500

    C commit

--- Mario/Mario.c
+++ Mario/Mario.c
@@ -26,38 +26,38 @@
 
 
 /////////////////////////////////////////////////////////
-#include <cs50.h>
-#include <stdio.h>
+// #include <cs50.h>
+// #include <stdio.h>
 
-int main(void)
-{
-    int height, row, column, spacing;
+// int main(void)
+// {
+//     int height, row, column, spacing;
 
-    // Prompt the user to enter a height between 1 and 8
-    do
-    {
-        height = get_int("Enter Height: ");
-    }
-    while (height < 1 || height > 8);
+//     // Prompt the user to enter a height between 1 and 8
+//     do
+//     {
+//         height = get_int("Enter Height: ");
+//     }
+//     while (height < 1 || height > 8);
     
-    // Loop over each row
-    for (row = 0; row < height; row++)
-    {
-        // Print the spaces
-        for (spacing = 0; spacing < height - row - 1; spacing++)
-        {
-            printf(" ");
-        }
+//     // Loop over each row
+//     for (row = 0; row < height; row++)
+//     {
+//         // Print the spaces
+//         for (spacing = 0; spacing < height - row - 1; spacing++)
+//         {
+//             printf(" ");
+//         }
 
-        // Print the hashes
-        for (column = 0; column <= row; column++)
-        {
-            printf("#");
-        }
+//         // Print the hashes
+//         for (column = 0; column <= row; column++)
+//         {
+//             printf("#");
+//         }
 
-        // Move to the next line after each row
-        printf("\n");
-    }
+//         // Move to the next line after each row
+//         printf("\n");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
