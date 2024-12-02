581999275db526e78e6947a3261f3a39f1c5ec7e
Sumair javed <sumairjaved007@gmail.com>
2024-12-02 17:06:37 +0500

    Funtions

---
+++ pythoncoding/Funtions.py
@@ -0,0 +1,16 @@
+# def gmean(a, b):
+#     mean = a*b / a+b
+#     print(mean)
+
+# a= 23
+# b = 24
+# gmean(a, b)
+
+# if a > b:
+#     print("A is greater")
+# else:
+#     print("B is greater")
+
+#///////////////////////////////////
+
+
